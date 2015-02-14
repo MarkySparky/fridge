e001848fb405712b7e90de699a60da6681fb119a
Mark Caulfield <markacaulfield@googlemail.com>
2015-02-14 16:00:24 +0000

    task initial home screen

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,9 +4,33 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('Fridge', ['ionic', 'config'])
 
-.run(function($ionicPlatform) {
+
+var app = angular.module('Fridge', ['ionic', 'config']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider.state('home', {
+    url: '/home',
+    views: {
+      home: {
+        templateUrl: 'templates/home.html'
+      }
+    }
+  });
+
+  $stateProvider.state('help', {
+    url: '/help',
+    views: {
+      help: {
+        templateUrl: 'templates/help.html'
+      }
+    }
+  });
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
